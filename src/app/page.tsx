dbce2a61175d70cf074a28b9386056ebba158a17
ADevph <das.atanu42914@gmail.com>
2024-03-17 15:51:11 +0600

    featuredcourses completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -6,6 +7,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while (/[0.02]) ">
 
 <Hero />
+
+<FeaturedCourses />
 
     </main>
 
