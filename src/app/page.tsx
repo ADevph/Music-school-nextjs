24fd73bb2cc1b6eaa09ee25f412d5c97b0db6d32
ADevph <das.atanu42914@gmail.com>
2024-03-17 23:03:45 +0600

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Instructors from "@/components/Instructors";
 import Scroll from "@/components/Scroll";
@@ -9,12 +10,12 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while (/[0.02]) ">
       <Hero />
-
       <FeaturedCourses />
       <Scroll />
       <TestimonialCards />
       <UpcomingEvents />
       <Instructors />
+      <Footer />
     </main>
   );
 }
