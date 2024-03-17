62ce75f8c06a1c5c0f431e7e918b8ea163c97c9f
ADevph <das.atanu42914@gmail.com>
2024-03-17 12:55:26 +0600

    herosection completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
   
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while (/[0.02]) ">
 
-Music Home Page
+<Hero />
 
     </main>
 
